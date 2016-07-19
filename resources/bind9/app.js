0966cd4ed9b71dbb9e1efb0f7e1b92f140f66e00
Hong, Dewey <doohee323@gmail.com>
2016-07-18 23:33:13 -0700

    .

--- resources/bind9/app.js
+++ resources/bind9/app.js
@@ -1,5 +1,3 @@
-'use strict';
-
 var http = require('http');
 var os = require("os");
 
--- scripts/bind9/build.sh
+++ scripts/bind9/build.sh
@@ -2,23 +2,50 @@
 
 set -x
 
-### [make test bind9 docker image] ###########################################################################################
+### [make test bind9 docker image] ###############################################################
 cd /home/vagrant
 sudo docker rmi testbind9:0.1
 sudo cp -Rf /vagrant/resources/bind9 /home/vagrant
 cd bind9
 
-export ip=$(ip addr show eth0 | grep -Po 'inet \K[\d.]+')
-sudo echo $ip 'registry.tz.com' >> /etc/hosts
-#docker run -it --add-host registry.tz.com:"$ip" ubuntu cat /etc/hosts
-
 sudo docker build --tag testbind9:0.1 .
-sudo docker images
+#sudo docker images
+
+# host ip
+host_ip=`ip addr show eth0 | grep -Po 'inet \K[\d.]+'`
+echo "====== host_ip:"$host_ip
 
 sudo docker stop bind93
 sudo docker rm bind93
+sudo docker run -d -p 3000:3000 --name bind93 \
+    testbind9:0.1
 
-sudo docker run -ti --name bind93 testbind9:0.1
+# container ip    
+docker-ip() {
+  docker inspect --format '{{ .NetworkSettings.IPAddress }}' "$@"
+}
+export cont_ip=`docker-ip bind93`
+echo "====== cont_ip:"$cont_ip
+
+# container network setting  172.17.0.10
+sudo docker stop bind93
+sudo docker rm bind93
+sudo docker run -d -p 3000:3000 --name bind93 \
+	--add-host registry.tz.com:$cont_ip \
+	--hostname nserver \
+	--dns $cont_ip --dns 8.8.8.8 --dns-search tz.com \
+    testbind9:0.1 &
+
+sudo docker exec -d bind93 /bin/sed -i "s/PUBLIC_IP/$cont_ip/g" /etc/bind/db.tz.com
+sudo docker exec -d bind93 service bind9 start
+
+sudo docker ps -a | grep bind93
+sudo docker exec -it bind93 /bin/bash
+dig @registry.tz.com tz.com
+exit
+
+# exitable: ctrl + c 
+#sudo docker attach --sig-proxy=false bb650abb87d1
 
 ### [pull test image from external server with https] ##########################################################################
 sudo docker login --username=testuser --password=testpassword
