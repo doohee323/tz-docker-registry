f482b151ac7ccdb6cba39781214914f687d716a1
doohee323 <doohee323@gmail.com>
2019-05-22 22:07:29 -0700

    .

--- scripts/registry.sh
+++ scripts/registry.sh
@@ -44,10 +44,63 @@ services:
       - ./data:/data
 EOF
 
+
+
+
+
+### [nginx] ############################################################################################################
 apt-get install nginx -y
 
-cd ~/docker-registry
-docker-compose up
+export ip=$(ip addr show eth1 | grep -Po 'inet \K[\d.]+')
+
+# chown -Rf vagrant:vagrant /etc/hosts
+sh -c "echo '' >> /etc/hosts"
+if [ "$TEST_Y" != "" ]; then  # test
+	 sh -c "echo '192.168.82.170 registry.tz.com' >> /etc/hosts"
+else
+	 sh -c "echo '$ip registry.tz.com' >> /etc/hosts"
+fi
+
+### [update certs] ############################################################################################################
+cp /vagrant/domain.crt /usr/share/ca-certificates/
+echo "domain.crt" |  tee -a /etc/ca-certificates.conf
+ update-ca-certificates
+
+### [docker-registry] ##################################################################################################
+mkdir -p certs
+ mkdir -p /certs
+ cp /vagrant/domain.* /certs
+
+ mkdir auth
+ docker run --entrypoint htpasswd registry:2 -Bbn testuser testpassword > auth/htpasswd
+
+ docker stop registry
+ docker rm registry
+ docker run -d --restart=always -p 5000:5000 --name registry \
+  -v `pwd`/auth:/auth \
+  -e "REGISTRY_AUTH=htpasswd" \
+  -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
+  -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
+  -v `pwd`/certs:/certs \
+  -v /vagrant/domain.crt:/certs/domain.crt \
+  -v /vagrant/domain.key:/certs/domain.key \
+  -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt \
+  -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key \
+  registry:2
+
+ docker ps -a
+
+# install shipyard
+curl -sSL https://shipyard-project.com/deploy | bash -s
+
+# for test building image
+if [ "$TEST_Y" != "" ]; then  # test
+	echo "build test image"
+	bash /vagrant/scripts/nodejs/build.sh
+	bash /vagrant/scripts/nginx/build.sh
+	#bash /vagrant/scripts/bind9/build.sh
+fi
+
 
 
 exit 0
